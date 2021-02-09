22f5999893f429a809ba06c3032c854ac0cde272
Kaken Bok <bok@kakenbok.de>
2021-02-09 10:04:36 +0100

    add dependend libs

--- .afeefa/package/release/release.php
+++ .afeefa/package/release/release.php
@@ -2,9 +2,19 @@
 
 use Afeefa\Component\Package\Package\Package;
 use Afeefa\Component\Package\ReleaseManager;
+use Webmozart\PathUtil\Path;
 
 return (new ReleaseManager())
     ->packages([
         Package::composer()
-            ->path(getcwd())
+            ->path(getcwd()),
+
+        Package::composer()
+            ->path(Path::join(getcwd(), '..', 'cli-app')),
+
+        Package::composer()
+            ->path(Path::join(getcwd(), '..', 'debug-dump-log')),
+
+        Package::composer()
+            ->path(Path::join(getcwd(), '..', 'package-manager'))
     ]);
