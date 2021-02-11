47c347021338ee5f4d9f6a2f69d3a88aed4289c7
Kaken Bok <bok@kakenbok.de>
2021-02-11 12:01:49 +0100

    update release packages

---
+++ .afeefa/package/release/packages.php
@@ -0,0 +1,15 @@
+<?php
+
+use Afeefa\Component\Package\Package\Package;
+use Webmozart\PathUtil\Path;
+
+return [
+    Package::composer()
+        ->path(Path::join(getcwd(), '..', 'cli-app')),
+
+    Package::composer()
+        ->path(Path::join(getcwd(), '..', 'debug-dump-log')),
+
+    Package::composer()
+        ->path(Path::join(getcwd(), '..', 'package-manager'))
+];
